db798a78c6becf325d4cb8e35b832bc084c96ba5
learnsty <info@collegemobile.net>
2016-09-19 14:05:16 +0100

    Create App.php

---
+++ c_app/core/App.php
@@ -0,0 +1,63 @@
+<?php
+class App{
+	protected $controller="";
+	protected $method ="index";
+	protected $params = array();
+
+	public function __construct(){
+
+		$url=$this->parseUrl();
+        $dirlocation="http://".$_SERVER['HTTP_HOST']."/".$url[0]."/";
+		if(file_exists("c_app/controllers/".$url[0].".php")){
+            
+			$this->controller=$url[0];
+		}
+        else{
+            if($url[0]!=''){
+                           
+            $this->controller='content';    
+        
+            
+            }
+			
+            else{
+             $this->controller='home';       
+            }
+
+            
+        }
+        
+        
+        
+		//unset($url[0]);
+		require_once('c_app/controllers/'.$this->controller.'.php');
+      
+		$this->controller = new $this->controller($url,$dirlocation);
+        
+        ////// If the url contains a method
+		if(isset($url[1])){
+			if(method_exists($this->controller, $url[1])){
+				$this->method=$url[1];
+				
+			}
+		unset($url[1]);	
+
+		}	
+		
+        
+		$this->params =$url ? array_values($url) : array();
+		call_user_func_array(array($this->controller,$this->method), $this->params);
+	}
+
+
+	public function parseUrl(){
+
+		if(isset($_GET['url'])){
+			return $url=explode('/',filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
+
+		}
+	}
+}
+
+
+?>
